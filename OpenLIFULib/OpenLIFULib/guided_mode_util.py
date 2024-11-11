a30e8a68adfecddf206d4b88c16d8c50b4abbdbf
Sadhana Ravikumar <sadhana.ravikumar@kitware.com>
2024-11-11 11:22:57 -0500

    Add function to set guided mode state (#126)

--- OpenLIFULib/OpenLIFULib/guided_mode_util.py
+++ OpenLIFULib/OpenLIFULib/guided_mode_util.py
@@ -1,7 +1,11 @@
 import slicer
 
-def get_guided_mode_status() -> bool:
-    """Get guided mode status from the OpenLIFU Home module's parameter node"""
+def get_guided_mode_state() -> bool:
+    """Get guided mode state from the OpenLIFU Home module's parameter node"""
     openlifu_home_parameter_node = slicer.util.getModuleLogic('OpenLIFUHome').getParameterNode()
     return openlifu_home_parameter_node.guided_mode
 
+def set_guided_mode_state(new_guided_mode_state: bool):
+    """Set guided mode state in OpenLIFU Home module's parameter node"""
+    openlifu_home_parameter_node = slicer.util.getModuleLogic('OpenLIFUHome').getParameterNode()
+    openlifu_home_parameter_node.guided_mode = new_guided_mode_state
